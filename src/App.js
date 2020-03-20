172831bab5ab466d8eac65ba240de71712abe804
raflorenz <rjlorenz931@gmail.com>
2020-03-21 01:09:45 +0800

    add reducer function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 
+function appReducer(state, action) {
+  switch (action.type) {
+    case 'add':
+      return [
+        ...state,
+        {
+          id: Date.now(),
+          title: 'todo added',
+          completed: false
+        }
+      ]
+    default:
+      return state;
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(appReducer, []);
+
   return (
     <div className="App">
       <h1>Moto</h1>
+      <button onClick={() => dispatch({ type: 'add' })}>Add todo</button>
+      {state.map(item => (
+        <div key={item.id}>{item.title}</div>
+      ))}
     </div>
   );
 }
